9b1fed038b68462d1cd3ab03edbd2b60b4a80d1f
八荒 <18705698875@163.com>
2021-07-30 10:43:34 +0800

    fix: 新增发布订阅，事件监听方法, 新增删除指定事件指定方法

--- src/eventChange/index.js
+++ src/eventChange/index.js
@@ -14,9 +14,29 @@ class EventChange {
         if (!fns || fns.length === 0) {
             return
         }
-        let fn;
-        for (let i = 0; fn = fns[i]; i++) {
+        let fn
+        for (let i = 0; i < fns.length; i++) {
+            fn = fns[i]
             fn.apply(this, arguments)
+        }
+    }
+
+    remove(key, fn) {
+        let fns = this.list[key]
+        if (!fns) {
+            return
+        }
+        if (!fn) {
+            // 默认删除所有方法
+            fns.length = 0
+        } else {
+            // 删除指定事件指定方法
+            for (let len = fns.length - 1; len >= 0; len--) {
+                let _fn = fns[len]
+                if (_fn === fn) {
+                    fns.splice(len, 1)
+                }
+            }
         }
     }
 }
