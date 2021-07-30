7e187defb45a9e94d2de014babc5edfc6330177c
八荒 <18705698875@163.com>
2021-07-30 10:30:47 +0800

    fix: 新增发布订阅，事件监听方法

---
+++ src/publishSubscribe/EventChange.js
@@ -0,0 +1,24 @@
+class EventChange {
+    list = {}
+
+    on(key, fn) {
+        if (!this.list[key]) {
+            this.list[key] = []
+        }
+        this.list[key].push(fn)
+    }
+
+    emit() {
+        let key = Array.prototype.shift.call(arguments)
+        let fns = this.list[key]
+        if (!fns || fns.length === 0) {
+            return
+        }
+        let fn;
+        for (let i = 0; fn = fns[i]; i++) {
+            fn.apply(this, arguments)
+        }
+    }
+}
+
+export default new EventChange()
